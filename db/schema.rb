eb7b1c8d336c60b85683813a6c50f7c91fcaeb46
Joseph Lepis <jlepis2010@gmail.com>
2018-08-31 09:37:34 -1000

    Added user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_30_184953) do
+ActiveRecord::Schema.define(version: 2018_08_31_193028) do
 
   create_table "quotes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "quote"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2018_08_30_184953) do
     t.string "title"
     t.text "description"
     t.string "created_by"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
