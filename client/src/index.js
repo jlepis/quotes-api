ab867b04a70495184d5743248ea51988ca510839
Joseph Lepis <jlepis2010@gmail.com>
2018-09-01 16:15:51 -1000

    possible fix to get admin working on heroku

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import 'semantic-ui-css/semantic.css'
 import './index.css';
 import App from './App';
 
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
